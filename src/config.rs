b9ab3f9fc2096c612ce675fd7945dc2c0f75c9e5
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2023-08-19 15:18:04 -0700

    resize dimensions

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,11 @@
 // Game configuration parameters
 
-pub const WIDTH: usize = 200;
-pub const HEIGHT: usize = 120;
+pub const WIDTH: usize = 240;
+pub const HEIGHT: usize = 150;
 
-pub const BALL_SIZE: usize = 2;
-pub const PADDLE_WIDTH: usize = 2;
-pub const PADDLE_HEIGHT: usize = 20;
+pub const BALL_SIZE: usize = 3;
+pub const PADDLE_WIDTH: usize = 3;
+pub const PADDLE_HEIGHT: usize = 25;
 
 pub const COLOR: u8 = 0xFF;
 pub const WINDOW_SCALE: usize = 4;

f99263dffd82f92c07701354b56deb231002a298
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2023-08-19 18:16:10 -0700

    add draw ball methods

--- src/core/frame.rs
+++ src/core/frame.rs
@@ -153,11 +153,40 @@ impl Frame {
 
     // Draw ball on internal frame at position with subpixel rendering
 
-    fn draw_ball_internal(frame: &mut [u8], pos: FloatPoint, color: u8) {}
+    fn draw_ball_internal(frame: &mut [u8], pos: FloatPoint, color: u8) {
+        let (start_pos, ball_pixels) = Self::calc_ball_pixels(pos, color);
+        for row in 0..BALL_SIZE + 1 {
+            for col in 0..BALL_SIZE + 1 {
+                let offset = (start_pos.0 + row) * WIDTH + start_pos.1 + col;
+                frame[offset] = ball_pixels[row][col];
+            }
+        }
+    }
 
     // Draw ball on Pixels RGBA frame at position with subpixel rendering
 
-    fn draw_ball_display(rgba_grame: &mut [u8], pos: FloatPoint, color: u8) {}
+    fn draw_ball_display(rgba_frame: &mut [u8], pos: FloatPoint, color: u8) {
+        let (start_pos, ball_pixels) = Self::calc_ball_pixels(pos, color);
+        for row in 0..BALL_SIZE + 1 {
+            for col in 0..BALL_SIZE + 1 {
+                let offset = ((start_pos.0 + row) * WIDTH + start_pos.1 + col) * 4;
+                rgba_frame[offset] = ball_pixels[row][col];
+                rgba_frame[offset + 1] = ball_pixels[row][col];
+                rgba_frame[offset + 2] = ball_pixels[row][col];
+                rgba_frame[offset + 3] = 0xFF;
+            }
+        }
+    }
+
+    // Calculate pixel colors for ball subpixel rendering
+
+    fn calc_ball_pixels(
+        pos: FloatPoint,
+        color: u8,
+    ) -> (Point, [[u8; BALL_SIZE + 1]; BALL_SIZE + 1]) {
+        let start_pos = Point(pos.0.floor() as usize, pos.1.floor() as usize);
+        unimplemented!()
+    }
 
     // Draw rectangle on internal frame at position with width and height
 

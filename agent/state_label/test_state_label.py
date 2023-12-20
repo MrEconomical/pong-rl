68d4a659ff090f85c59276939fec754b6d6ac323
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2023-12-20 10:46:36 -0800

    add supervised stochastic model

---
+++ agent/state_label/test_state_label.py
@@ -0,0 +1,34 @@
+'''
+test script for models
+'''
+
+from pathlib import Path
+import sys
+sys.path.insert(0, str(Path(Path(__file__).parent.absolute()).parent.absolute()))
+
+from models.stochastic_model import Model
+import numpy as np
+import pong_rl
+
+checkpoint = 8
+save_folder = "stochastic_models"
+model = Model.from_save("agent/state_label/" + save_folder + "/" + str(checkpoint) + ".json")
+print("loaded model with parameters ({}, {}, {}, {}) from checkpoint {}".format(
+    model.input_size,
+    model.hidden_size,
+    model.output_size,
+    model.learning_rate,
+    checkpoint,
+))
+
+pong = pong_rl.PongEnv.with_render()
+pong.start()
+
+while True:
+    reward = 0
+    while reward == 0:
+        game_state = pong.get_normalized_state()
+        h, action_prob = model.forward(game_state)
+        action = 1 if np.random.uniform() < action_prob else 0
+        reward = pong.tick(action)
+    pong.reset()
